b3df080ab233a492a1368de5d37bad997eac1b9f
shalini <shalini.v@tvsnext.io>
2021-07-09 18:52:29 +0530

    [shalini]Insertion at last

---
+++ LinkedListOperation/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LinkedListOperation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //UseCase 1
+            Operations obj = new Operations();
+            //Insertion Operation
+            obj.InsertLast(56);
+            obj.InsertLast(30);
+            obj.InsertLast(70);
+            //Display Operation
+            Console.WriteLine("*****Insertion at Last*****");
+            obj.Display();
+        }
+    }
+}
