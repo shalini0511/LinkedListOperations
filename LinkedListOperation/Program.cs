5d25a100fdc13a7f8acee8268eb68035cffa74bd
shalini <shalini.v@tvsnext.io>
2021-07-10 12:46:52 +0530

    [shalini]Inserting in Miidle

--- LinkedListOperation/Program.cs
+++ LinkedListOperation/Program.cs
@@ -9,9 +9,9 @@ namespace LinkedListOperation
             //UseCase 1
             Operations obj = new Operations();
             //Insertion Operation
-            obj.Append(56);
-            obj.Append(30);
-            obj.Append(70);
+            obj.InsertLast(56);
+            obj.InsertBetween(2,30);
+            obj.InsertLast(70);
             //Display Operation
             Console.WriteLine("*****Appending*****");
             obj.Display();
